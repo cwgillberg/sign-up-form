7572162ebd52f5893bada2fe1ff40fa72842349a
Carl-William Gillberg <cwgillberg@gmail.com>
2023-02-17 10:49:40 +0100

    began password validation
    added initial password validation
    added dynamic button styling for when user can/cannot submit the form
    added initial DOM password error message

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
-/*
-TODO:
-Password validation
-
-
-
-*/
-
 const password = document.querySelector(".password");
 const confirmPassword = document.querySelector(".confirm-password");
 const button = document.querySelector("button");
+const passwordField = document.querySelector("#pass-error");
+const confirmPassField = document.querySelector("#confirm-error");
 
 confirmPassword.addEventListener("keyup", validatePassword);
 
+window.addEventListener("load", toggleButton("off"));
+
+/*
+VALIDATION PSEUDO CODE
+1. BUTTON IS DISABLED BY DEFAULT
+2. IF PASSWORDS MATCH -> ENABLE BUTTON
+3. IF PASSWORDS DO NOT MATCH -> DISPLAY ERROR
+4. WHEN PASSWORDS MATCH -> REMOVE ERROR, ENABLE BUTTON
+*/
+
+
 function validatePassword() {
     if(password.value === confirmPassword.value) {
-        console.log("ok");
+        toggleButton("on");
     } else {
-       button.textContent = "haha nope"
+        getError();
     }
+}
+
+function toggleButton(state) {
+    if(state === "on") {
+        button.disabled = "enabled"
+    } else {
+        button.disabled = "disabled"
+        button.style.opacity = 0.5;
+    }
+}
+
+
+function getError() {
+    let errorBody = document.createElement("p");
+    let errorText = document.createTextNode("Sorry! These passwords don't match");
+    errorBody.appendChild(errorText);
+    confirmPassField.appendChild(errorBody);
 }
